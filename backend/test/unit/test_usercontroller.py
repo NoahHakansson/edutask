2aa11e608872fd03bb3e85bcbe296bb00f9cfb06
Noah Hakansson <noah.hakansson@protonmail.com>
2022-04-12 20:27:27 +0200

    Added folders and files for the unit testing.

---
+++ backend/test/unit/test_usercontroller.py
@@ -0,0 +1,10 @@
+from ....backend.src.controllers.usercontroller import UserController
+
+import pytest
+import unittest.mock as mock
+
+def test_usercontroller_invalid_email_format():
+    mockedusercontroller = mock.MagicMock()
+
+
+
