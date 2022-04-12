f7dc450fdb209445ab258cfed7f0b69b89cd7975
Noah Hakansson <noah.hakansson@protonmail.com>
2022-04-12 21:07:15 +0200

    Added first test, its kinda wierd tho.

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -1,10 +1,16 @@
-from ....backend.src.controllers.usercontroller import UserController
+from src.controllers.usercontroller import UserController
 
 import pytest
 import unittest.mock as mock
 
 def test_usercontroller_invalid_email_format():
-    mockedusercontroller = mock.MagicMock()
+    mockedUserController = mock.MagicMock()
+    # return empty dict because we dont expect anything from a invalid email
+    mockedUserController.get.return_value = {}
 
+    sut = UserController(dao=mockedUserController)
+    validationResult = sut.get_user_by_email("mail")
+
+    assert validationResult == "test"
 
 
