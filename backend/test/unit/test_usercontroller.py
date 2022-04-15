cc99fa511871e8ba945854820cabf3e7e486d66f
DingusDungus <DingusDungus@github.com>
2022-04-15 14:24:18 +0200

    Written test cases for unit testing

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -3,15 +3,35 @@ from src.controllers.usercontroller import UserController
 import pytest
 import unittest.mock as mock
 
-def test_usercontroller_invalid_email_format():
+def test_usercontroller_invalid_email_format0():
+    mockedDatabase = mock.MagicMock()
+    # return empty dict because we dont expect anything from a invalid email
+    mockedDatabase.find.return_value = [{"email":"mail@mail.com"}]
+
+    sut = UserController(dao=mockedDatabase)
+    validationResult = sut.get_user_by_email("mail")
+
+    assert validationResult == {"email":"mail@mail.com"}
+
+def test_usercontroller_invalid_email_format1():
+    mockedDatabase = mock.MagicMock()
+    # return empty dict because we dont expect anything from a invalid email
+    mockedDatabase.find.return_value = [{"email":"mail@mail.com"}]
+
+    sut = UserController(dao=mockedDatabase)
+    validationResult = sut.get_user_by_email("mail@")
+
+    assert validationResult == {"email":"mail@mail.com"}
+
+def test_usercontroller_invalid_email_format2():
     mockedDatabase = mock.MagicMock()
     # return empty dict because we dont expect anything from a invalid email
     mockedDatabase.find.return_value = []
 
     sut = UserController(dao=mockedDatabase)
-    validationResult = sut.get_user_by_email("mail")
+    validationResult = sut.get_user_by_email("mail@mail")
 
-    assert validationResult == "test"
+    assert validationResult == {}
 
 def test_usercontroller_valid_email_format():
     mockedDatabase = mock.MagicMock()
@@ -21,4 +41,14 @@ def test_usercontroller_valid_email_format():
     sut = UserController(dao=mockedDatabase)
     validationResult = sut.get_user_by_email("mail@mail.com")
     assert validationResult == { "email": "mail@mail.com"}
+
+def test_usercontroller_not_in_database_format0():
+    mockedDatabase = mock.MagicMock()
+    # return empty dict because email is not in database
+    mockedDatabase.find.return_value = []
+
+    sut = UserController(dao=mockedDatabase)
+    validationResult = sut.get_user_by_email("mail@mail.com")
+
+    assert validationResult ==  {"email":"mail@mail.com"}
 
