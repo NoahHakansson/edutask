0d6247741d29c2b36e9a432e36d0c2aee6161573
Noah Hakansson <noah.hakansson@protonmail.com>
2022-04-19 21:06:40 +0200

    Just added commented out function that maybe we can use as a backup strategy.

--- backend/test/integration/test_dao.py
+++ backend/test/integration/test_dao.py
@@ -6,7 +6,17 @@ import mongomock
 import unittest.mock as mock
 from unittest.mock import patch
 
-MOCK_URL = "mongomock://localhost"
+MOCK_URL = "mock url goes here"
+
+# @pytest.fixture
+# def sut():
+#     # Mocks the actual database url that we connect to with MOCK_URL constant above
+#     with patch("src.util.dao.os", autospec=True) as mockedDAO:
+#         mockedDAO.environ.get.return_value = MOCK_URL
+#         # create DAO
+#         sut = dao("todo")
+#         sut.collection
+#         return sut
 
 @pytest.fixture
 def sut():
@@ -19,7 +29,6 @@ def sut():
 
 
 
-@pytest.mark.staging
 def test_todo_test0(sut):
     # test
     data = {"test0" : False}
