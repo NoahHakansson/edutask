49f28f4a9404c517ce2cc8eb3949eb92c2b3b436
DingusDungus <DingusDungus@github.com>
2022-04-20 12:43:24 +0200

    Test database done

--- backend/test/integration/test_dao.py
+++ backend/test/integration/test_dao.py
@@ -1,45 +1,26 @@
+import collections
 from typing import Collection
 from src.util.dao import DAO as dao
-from src.util.validators import getValidator
 import pytest
-import mongomock
 import unittest.mock as mock
-from unittest.mock import patch
+from test.utils.initTestCollections import testDatabase
 
-MOCK_URL = "mock url goes here"
+sut = testDatabase
+collections = sut.getDatabase()
 
-# @pytest.fixture
-# def sut():
-#     # Mocks the actual database url that we connect to with MOCK_URL constant above
-#     with patch("src.util.dao.os", autospec=True) as mockedDAO:
-#         mockedDAO.environ.get.return_value = MOCK_URL
-#         # create DAO
-#         sut = dao("todo")
-#         sut.collection
-#         return sut
-
-@pytest.fixture
-def sut():
-    # mock mongoDB
-    with patch("src.util.dao.pymongo", side_effect=mongomock.MongoClient) as mockedDatabase:
-        # create DAO
-        sut = dao("todo")
-        sut.collection
-        return sut
-
-
-
-def test_todo_test0(sut):
+def test_todo_test0():
     # test
     data = {"test0" : False}
-    result = sut.create(data)
+    
 
-    assert result == {"test": "test"}
+    assert data == {"test": "test"}
 
-def test_todo_test1(sut):
+def test_todo_test1():
     # test
     data = {5 : None}
-    result = sut.create(data)
+    
 
-    assert result == {"test": "test"}
+    assert data == {"test": "test"}
+
+sut.clearDatabase()
 
