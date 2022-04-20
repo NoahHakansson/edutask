4e8ded76f9b0e0f9397fd182ebc37b9083b37eab
Noah Hakansson <noah.hakansson@protonmail.com>
2022-04-20 14:10:28 +0200

    it worki :D

--- backend/test/integration/test_dao.py
+++ backend/test/integration/test_dao.py
@@ -3,23 +3,40 @@ from typing import Collection
 from src.util.dao import DAO as dao
 import pytest
 import unittest.mock as mock
-from test.utils.testDatabase import testDatabase
+from src.util.testDatabase import testDatabase
 
-collections = testDatabase.getDatabase()
 
-def test_todo_test0():
+@pytest.fixture
+def todo_sut():
+    # Fixture to return todo SUT
+    # create test collections
+    collections = testDatabase.getDatabase()
+    # create DAO and set it up with the test collection
+    sut = dao("todo")
+    sut.collection = collections["todo"]
+    yield sut
+    # clear database after test is run
+    testDatabase.clearDatabase()
+
+def test_todo_test0(todo_sut):
     # test
-    sut = dao("task")
-    sut.collection = collections['task']
-    
+    data = {"description":"test0"}
+    result = todo_sut.create(data)
 
-    assert {} == {"test": "test"}
+    assert result == {"test": "test"}
 
-def test_todo_test1():
+def test_todo_test1(todo_sut):
     # test
-    data = {5 : None}
-    
+    data = {"description":"test1", "done": False}
+    result = todo_sut.create(data)
 
-    assert data == {"test": "test"}
+    assert result == {"test": "test"}
+
+def test_todo_test2(todo_sut):
+    # test
+    data = {"description":5, "done": False}
+    result = todo_sut.create(data)
+
+    assert result == {"test": "test"}
 
 
