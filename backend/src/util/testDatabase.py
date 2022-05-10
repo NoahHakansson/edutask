93c451555426dbddfa4de406cbe351e23976b881
Noah Hakansson <noah.hakansson@protonmail.com>
2022-05-10 12:18:58 +0200

    Tests are remade, should be fine now and follows rapport document.

--- backend/src/util/testDatabase.py
+++ backend/src/util/testDatabase.py
@@ -10,11 +10,12 @@ from src.util.validators import getValidator
 import json
 from bson import json_util
 from bson.objectid import ObjectId
-collections = {}
+
+collection = None
 
 class testDatabase:
 
-    def getDatabase():
+    def getCollection():
          # load the local mongo URL (something like mongodb://localhost:27017)
             LOCAL_MONGO_URL = dotenv_values('.env').get('MONGO_URL')
             # check out of the environment (which can be overridden by the docker-compose file) also specifies an URL, and use that instead if it exists
@@ -23,27 +24,15 @@ class testDatabase:
             # connect to the MongoDB and select the appropriate database
             client = pymongo.MongoClient(MONGO_URL)
             database = client.edutask
-            # Todo scheme
-            if "test_todo" not in database.list_collection_names():
-                validator = getValidator("todo")
-                database.create_collection("test_todo", validator=validator)
-            # task scheme
-            if "test_task" not in database.list_collection_names():
-                validator = getValidator("task")
-                database.create_collection("test_task", validator=validator)
-            # user scheme
-            if "test_user" not in database.list_collection_names():
-                validator = getValidator("user")
-                database.create_collection("test_user", validator=validator)
-            # video scheme
-            if "test_video" not in database.list_collection_names():
-                validator = getValidator("video")
-                database.create_collection("test_video", validator=validator)
+            # scheme
+            if "test_collection" not in database.list_collection_names():
+                validator = getValidator("testValidator")
+                database.create_collection("test_collection", validator=validator)
             # add all collections to dict array
-            collections = {"todo": database["test_todo"], "task":database['test_task'], "user":database['test_user'], "video":database['test_video']}
+            collection = database["test_collection"]
             
             
-            return collections
+            return collection
 
     def clearDatabase():
         # load the local mongo URL (something like mongodb://localhost:27017)
@@ -53,8 +42,5 @@ class testDatabase:
             # connect to the MongoDB and select the appropriate database
         client = pymongo.MongoClient(MONGO_URL)
         database = client.edutask
-        database.drop_collection('test_todo')
-        database.drop_collection('test_task')
-        database.drop_collection('test_user')
-        database.drop_collection('test_video')
+        database.drop_collection('test_collection')
 
--- backend/test/integration/test_dao.py
+++ backend/test/integration/test_dao.py
@@ -12,11 +12,12 @@ from src.util.testDatabase import testDatabase
 @pytest.fixture
 def sut():
     # Fixture to return todo SUT
-    # create test collections
-    collections = testDatabase.getDatabase()
+    # create test collection
+    collection = testDatabase.getCollection()
     # create DAO and set it up with the test collection
-    sut = dao("testValidator")
-    sut.collection = collections["testValidator"]
+    sut = dao("todo") # todo just to create dao object. \
+    # Collection will be replaced below
+    sut.collection = collection
     yield sut
     # clear database after test is run
     testDatabase.clearDatabase()
@@ -25,99 +26,142 @@ def sut():
 ##### Tests #####
 ################
 
+def test_create_only_required(sut):
+    # test
+    data = {"string-prop":"test-string"}
+    result = sut.create(data)
+
+    assert result["string-prop"] == data["string-prop"]
+    
+def test_create_without_required(sut):
+    # test should throw Exception
+    with pytest.raises(Exception) as e_info:
+        data = {"boolean-prop":False}
+        sut.create(data)
+
+def test_create_duplicate_entry_of_string_prop(sut):
+    # test should throw Exception
+    data = {"string-prop":"already-existing-string"}
+    sut.create(data)
+    with pytest.raises(Exception) as e_info:
+        data = {"string-prop":"already-existing-string"}
+        sut.create(data)
+
+def test_create_both_fields_comply_bson_type(sut):
+    # test should throw Exception
+    data = {"string-prop":"this-is-a-string", "boolean-prop":True}
+    result = sut.create(data)
+
+    assert result["string-prop"] == data["string-prop"] \
+        and result["boolean-prop"] == data["boolean-prop"] 
+
+def test_create_string_field_not_comply_bson_type(sut):
+    # test should throw Exception
+    with pytest.raises(Exception) as e_info:
+        data = {"string-prop":1234, "boolean-prop":True}
+        sut.create(data)
+
+def test_create_boolean_field_not_comply_bson_type(sut):
+    # test should throw Exception
+    with pytest.raises(Exception) as e_info:
+        data = {"string-prop":"this-is-a-string", "boolean-prop":1234}
+        sut.create(data)
+
+
+
 # user SUT
-def test_user_test0(user_sut):
-    # test
-    data = {"firstName":"test", "lastName":"test", "email": "test"}
-    result = user_sut.create(data)
+# def test_user_test0(user_sut):
+#     # test
+#     data = {"firstName":"test", "lastName":"test", "email": "test"}
+#     result = user_sut.create(data)
 
-    assert result["firstName"] == data["firstName"] \
-        and result["lastName"] == data["lastName"] \
-        and result["email"] == data["email"]
+#     assert result["firstName"] == data["firstName"] \
+#         and result["lastName"] == data["lastName"] \
+#         and result["email"] == data["email"]
 
-def test_user_test1(user_sut):
-    # test should throw Exception
-    with pytest.raises(Exception) as e_info:
-        data = {"firstname":5, "lastname":"test", "email": "test"}
-        result = user_sut.create(data)
-
-
-def test_user_test2(user_sut):
-    # test should throw Exception
-    with pytest.raises(Exception) as e_info:
-        data = {"lastname":"test", "email": "test"}
-        result = user_sut.create(data)
+# def test_user_test1(user_sut):
+#     # test should throw Exception
+#     with pytest.raises(Exception) as e_info:
+#         data = {"firstname":5, "lastname":"test", "email": "test"}
+#         result = user_sut.create(data)
 
 
-# task SUT
-def test_task_test0(task_sut):
-    # test
-    data = {"title":"title","description":"test0"}
-    result = task_sut.create(data)
-
-    assert result["title"] == data["title"] \
-        and result["description"] == data["description"]
-
-def test_task_test1(task_sut):
-    # test should throw Exception
-    with pytest.raises(Exception) as e_info:
-        data = {"description":"test1"}
-        result = task_sut.create(data)
+# def test_user_test2(user_sut):
+#     # test should throw Exception
+#     with pytest.raises(Exception) as e_info:
+#         data = {"lastname":"test", "email": "test"}
+#         result = user_sut.create(data)
 
 
-def test_task_test2(task_sut):
-    # test should throw Exception
-    with pytest.raises(Exception) as e_info:
-        data = {"title":5,"description":5}
-        result = task_sut.create(data)
+# # task SUT
+# def test_task_test0(task_sut):
+#     # test
+#     data = {"title":"title","description":"test0"}
+#     result = task_sut.create(data)
+
+#     assert result["title"] == data["title"] \
+#         and result["description"] == data["description"]
+
+# def test_task_test1(task_sut):
+#     # test should throw Exception
+#     with pytest.raises(Exception) as e_info:
+#         data = {"description":"test1"}
+#         result = task_sut.create(data)
 
 
-# todo SUT
-def test_todo_test0(todo_sut):
-    # test
-    data = {"description":"test0"}
-    result = todo_sut.create(data)
-
-    assert result["description"] == data["description"] 
-
-def test_todo_test1(todo_sut):
-    # test
-    data = {"description":"test1", "done": False}
-    result = todo_sut.create(data)
-
-    assert result["description"] == data["description"] \
-        and result["done"] == data["done"]
-
-def test_todo_test2(todo_sut):
-    # test should throw Exception
-    with pytest.raises(Exception) as e_info:
-        data = {"description":5, "done": False}
-        result = todo_sut.create(data)
+# def test_task_test2(task_sut):
+#     # test should throw Exception
+#     with pytest.raises(Exception) as e_info:
+#         data = {"title":5,"description":5}
+#         result = task_sut.create(data)
 
 
-# video SUT
-def test_video_test0(video_sut):
-    # test
-    data = {"url":"test"}
-    result = video_sut.create(data)
+# # todo SUT
+# def test_todo_test0(todo_sut):
+#     # test
+#     data = {"description":"test0"}
+#     result = todo_sut.create(data)
 
-    assert result["url"] == data["url"]
+#     assert result["description"] == data["description"] 
 
-def test_video_test1(video_sut):
-    # test should throw Exception
-    with pytest.raises(Exception) as e_info:
-        data = {"url": 5}
-        result = video_sut.create(data)
+# def test_todo_test1(todo_sut):
+#     # test
+#     data = {"description":"test1", "done": False}
+#     result = todo_sut.create(data)
 
-# test uniqueItems flag / duplicate item
-def test_uniqueItems_task(task_sut):
-    # test should throw Exception
+#     assert result["description"] == data["description"] \
+#         and result["done"] == data["done"]
 
-    data = {"title":"title","description":"test0","categories":["test"]}
-    result = task_sut.create(data)
+# def test_todo_test2(todo_sut):
+#     # test should throw Exception
+#     with pytest.raises(Exception) as e_info:
+#         data = {"description":5, "done": False}
+#         result = todo_sut.create(data)
 
-    # try to insert/create duplicate entry with 'uniqueItems = true' property
-    with pytest.raises(Exception) as e_info:
-        data = {"title":"title","description":"test0","categories":["test"]}
-        result = task_sut.create(data)
+
+# # video SUT
+# def test_video_test0(video_sut):
+#     # test
+#     data = {"url":"test"}
+#     result = video_sut.create(data)
+
+#     assert result["url"] == data["url"]
+
+# def test_video_test1(video_sut):
+#     # test should throw Exception
+#     with pytest.raises(Exception) as e_info:
+#         data = {"url": 5}
+#         result = video_sut.create(data)
+
+# # test uniqueItems flag / duplicate item
+# def test_uniqueItems_task(task_sut):
+#     # test should throw Exception
+
+#     data = {"title":"title","description":"test0","categories":["test"]}
+#     result = task_sut.create(data)
+
+#     # try to insert/create duplicate entry with 'uniqueItems = true' property
+#     with pytest.raises(Exception) as e_info:
+#         data = {"title":"title","description":"test0","categories":["test"]}
+#         result = task_sut.create(data)
 
